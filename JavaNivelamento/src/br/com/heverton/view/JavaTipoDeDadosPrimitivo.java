efd21867e986a3f77c2d5bc16c0abb0a171128ed
hevertonaraujomachado <hevertonaraujomachado@gmail.com>
2023-02-17 09:23:14 -0300

    Atividade01

---
+++ JavaNivelamento/src/br/com/heverton/view/JavaTipoDeDadosPrimitivo.java
@@ -0,0 +1,24 @@
+package br.com.heverton.view;
+
+public class JavaTipoDeDadosPrimitivo {
+	public static void main(String[] args) {
+		System.out.println("Aplicação Java SE, Aula Nivelamento iniciando");
+//boolean - true false 8bits
+		boolean ativo = true;
+//byte --128 e 127 8bits
+		byte processo = 20;
+// short --32.768 e 32767 16 bits
+		short valorQualquer = 30000;
+// int -32 bits (2.147.483.647)
+		int teclado = 214040000;
+// long -64 bits
+		long muitogrande = 1116l;
+//float
+		float valorfloat = 120087f;
+//doble
+		double valordouble = 50000;
+	}
+
+	}
+
+
---
+++ JavaNivelamento/src/br/com/heverton/view/PrincipalDopacote.java
@@ -0,0 +1,11 @@
+package br.com.heverton.view;
+
+public class PrincipalDopacote {
+
+	public static void main(String[] args) {
+		System.out.println("Aplicação Java SE, Aula Nivelamento iniciando");
+
+
+	}
+
+}
